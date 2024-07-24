eef87070f7d6690ce82030cbe94511c1dbe31081
LSAabc <anan15919991635@163.com>
2024-07-24 19:42:32 +0800

    kkk

--- download.py
+++
@@ -1,4 +0,0 @@
-#模型下载
-from modelscope import snapshot_download
-model_dir = snapshot_download('iic/speech_sambert-hifigan_tts_zhiyan_emo_zh-cn_16k')
-print(model_dir)
--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-# res = requests.post("http://127.0.0.1:8000/api/chat", json={"input": "你好"}, timeout=600)
-
-res = requests.post("http://127.0.0.1:8000/api/tts", json={"text": ''' 潜力无限，塑造ColudAI'''})
-print(res.json())
-
